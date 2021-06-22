f150955241b5bd06a6179af2a81eaa4bef37d934
Yago Ananias <yago_souza92@yahoo.com.br>
2021-06-22 06:48:50 -0300

    Testando requisições GET e POST

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/teste-get", (request, response) => {
+  return response.send("Response get OK!")
+});
+
+app.post("/teste-post", (request, response) => {
+  return response.send("Response post OK!")
+});
+
 app.listen(3000, () => console.log("Deu bom!"));
